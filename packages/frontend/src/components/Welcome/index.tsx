6e1f7534474cffab8db8397cfa203d5dcdf758af
Krishi Shah <krishikshah@gmail.com>
2018-04-04 15:50:51 +0100

    Implement card based layout with menu bar on top and transitional trade steps

--- packages/frontend/src/components/Welcome/index.tsx
+++ packages/frontend/src/components/Welcome/index.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
+import { Container } from 'semantic-ui-react';
 
 interface Props {}
 
 export default ({  }: Props) => (
-    <div>
+    <Container textAlign="center">
         <h1>
             <img src="https://github.com/0xProject/branding/raw/master/0x_Black_CMYK.png" width="200px" />
             <br />
-            <br /> Welcome to the Sandb0x!
+            <br /> Welcome to the 0xygen!
         </h1>
-        The 0x.js sandbox provides a runnable example of how to interact with 0x.js packages. Be sure to check out the{' '}
-        <a href="https://0xproject.com/wiki">0x.js wiki</a> and the{' '}
-        <a href="https://github.com/0xProject/0x.js-starter-project">0x.js starter project </a>.
-    </div>
+    </Container>
 );
--- packages/frontend/src/containers/App/index.tsx
+++ packages/frontend/src/containers/App/index.tsx
@@ -3,10 +3,11 @@ import Welcome from '../../components/Welcome';
 import Account from '../Account';
 import Web3Actions from '../../components/Web3Actions';
 import Faucet from '../../components/Faucet';
+import { Dashboard } from '../../components/Sidebar/dashboard';
 import InstallMetamask from '../../components/InstallMetamask';
 import * as Web3 from 'web3';
 import * as RPCSubprovider from 'web3-provider-engine/subproviders/rpc';
-
+import { Divider, Container, Segment, Card, Step, Icon } from 'semantic-ui-react';
 import { InjectedWeb3Subprovider } from '@0xproject/subproviders';
 import { ZeroEx } from '0x.js';
 
@@ -23,31 +24,76 @@ const TEST_RPC = 'http://localhost:8545';
 
 const styles = {};
 
-const App = () => {
+interface Props { }
+
+export default class App extends React.Component<Props, {}> {
+
+    constructor(props: Props) {
+        super(props);
+    }
+
+    render() {
     // Detect if Web3 is found, if not, ask the user to install Metamask
     // tslint:disable-next-line:no-any
-    if ((window as any).web3) {
-        // Set up Web3 Provider Engine with a few helper Subproviders from 0x
-        const providerEngine = new Web3ProviderEngine();
-        providerEngine.addProvider(new InjectedWeb3Subprovider((window as any).web3.currentProvider));
-        providerEngine.addProvider(new RPCSubprovider({ rpcUrl: TEST_RPC }));
-        providerEngine.start();
+        if ((window as any).web3) {
+            // Set up Web3 Provider Engine with a few helper Subproviders from 0x
+            const providerEngine = new Web3ProviderEngine();
+            providerEngine.addProvider(new InjectedWeb3Subprovider((window as any).web3.currentProvider));
+            providerEngine.addProvider(new RPCSubprovider({ rpcUrl: KOVAN_RPC }));
+            providerEngine.start();
 
-        const web3 = new Web3(providerEngine);
-        // Initialize 0x.js with the web3 current provider and provide it the network
-        const zeroEx = new ZeroEx(web3.currentProvider, { networkId: TEST_RPC_NETWORK_ID });
-        // Browse the individual files for more handy examples
-        return (
-            <div style={styles}>
-                <Welcome />
-                <Account web3={web3} zeroEx={zeroEx} />
-                <Faucet zeroEx={zeroEx} />
-                <Web3Actions web3={web3} />
-            </div>
-        );
-    } else {
-        return <InstallMetamask />;
+            const web3 = new Web3(providerEngine);
+            // Initialize 0x.js with the web3 current provider and provide it the network
+            const zeroEx = new ZeroEx(web3.currentProvider, { networkId: KOVAN_NETWORK_ID });
+            // Browse the individual files for more handy examples
+            return (
+
+                <div style={{ padding: '3em'}}>
+                    <Dashboard/>
+                    <Welcome />
+                    
+                    <Card raised={true} centered={true} style={{ padding: '2em', width: '800px' }}>
+                        <Card.Content>
+                            <Card.Header>
+                                <Step.Group attached="top" widths={3} style={{ padding: '0em' }}>
+                                    <Step active>
+                                        <Icon name="pencil" />
+                                        <Step.Content>
+                                        <Step.Title>Allowances</Step.Title>
+                                        <Step.Description>Set Allowances for tokens you'd like to trade</Step.Description>
+                                        </Step.Content>
+                                    </Step>
+                                    <Step disabled>
+                                        <Icon name="exchange" />
+                                        <Step.Content>
+                                        <Step.Title>Trade</Step.Title>
+                                        <Step.Description>Exchange Tokens at a given rate</Step.Description>
+                                        </Step.Content>
+                                    </Step>
+                                    <Step disabled>
+                                        <Icon name="check" />
+                                        <Step.Content>
+                                        <Step.Title>Confirm Transaction</Step.Title>
+                                        <Step.Description>Transaction Receipt</Step.Description>
+                                        </Step.Content>
+                                    </Step>
+                                </Step.Group>
+                            </Card.Header>
+                        </Card.Content>
+                        <Card.Content>
+                            <Account web3={web3} zeroEx={zeroEx} />
+                        </Card.Content>
+                        <Card.Content>
+                            <Faucet zeroEx={zeroEx} />
+                        </Card.Content>
+                        <Card.Content>
+                            <Web3Actions web3={web3} />
+                        </Card.Content>
+                    </Card>
+                </div>
+            );
+        } else {
+            return <InstallMetamask />;
+        }
     }
-};
-
-export default App;
+}
